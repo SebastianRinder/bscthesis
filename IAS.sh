17ca0443dfce5843b4d424db993f365a81526668
Sebastian Rinder <sebastian.rinder@gmail.com>
2017-12-30 11:42:05 +0100

    .

---
+++ IAS.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+# COMPILE
+pdflatex thesis.tex
+pdflatex thesis.tex
+makeglossaries thesis
+makeglossaries thesis
+bibtex thesis
+pdflatex thesis.tex
+pdflatex thesis.tex
+
+# CLEAN UP
+shopt -s extglob nocaseglob
+for file in !(@(*.sty|*.pdf|*.tex|*.sh)); do
+    [[ -f "${file}" ]] && files+=( "${file}" )
+done
+(( ${#files[@]} )) && rm "${files[@]}"
