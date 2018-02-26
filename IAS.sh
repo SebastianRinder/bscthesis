a8df1b604442e0cc8346c5703b2eb35df86d8ec9
Sebastian Rinder <sebastian.rinder@gmail.com>
2018-02-26 12:03:38 +0100

    .

--- IAS.sh
+++ IAS.sh
@@ -9,8 +9,8 @@ pdflatex thesis.tex
 pdflatex thesis.tex
 
 # CLEAN UP
-shopt -s extglob nocaseglob
-for file in !(@(*.sty|*.pdf|*.tex|*.sh)); do
-    [[ -f "${file}" ]] && files+=( "${file}" )
-done
-(( ${#files[@]} )) && rm "${files[@]}"
+#shopt -s extglob nocaseglob
+#for file in !(@(*.sty|*.pdf|*.tex|*.sh)); do
+#    [[ -f "${file}" ]] && files+=( "${file}" )
+#done
+#(( ${#files[@]} )) && rm "${files[@]}"
